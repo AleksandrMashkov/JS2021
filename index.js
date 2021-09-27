119ce3bea8206fe8277669a18dd2780ca52d6504
Aleksandr Mashkov <leonardodv_86@inbox.ru>
2021-09-27 19:33:44 +0300

    lesson1

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log("Hello world");
+
+const NameYourPlanet = "Земля";
+let CurrektTimeUser = "23:00";
+let articleCount = 22;
+let isPaymentCash = "Оплата наличными или нет?";
+let FirstName = "Александр";
+let MiddleName = "Евгеньевич";
+let Surname = "Машков";
+
+console.log(NameYourPlanet);
+console.log(CurrektTimeUser);
+console.log(articleCount);
+console.log(isPaymentCash);
+console.log(FirstName);
+console.log(MiddleName);
+console.log(Surname);
