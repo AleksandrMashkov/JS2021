0f5a8ac911deac3d039c3c65cfd7bb1aceee54fa
Aleksandr Mashkov <leonardodv_86@inbox.ru>
2021-10-09 11:10:33 +0300

    cw

---
+++ JS/lesson4/index.js
@@ -0,0 +1,95 @@
+// if (true){
+//     console.log("TRUE");
+
+// }
+// if(0){
+//     console.log("FALSE");
+// }
+// console.log("start");
+// const value =-5;
+// if(value>0){
+//     console.log("true");
+//  } else{
+//      console.log("false");
+//  }
+//  console.log("finish");
+
+
+
+// console.log("start");
+// const value =5;
+// if(value>0){
+//     console.log("true");
+//  } else{
+//      console.log("false");
+//  }
+//  console.log("finish");
+
+
+
+
+// console.log("start");
+// let value =0 ;
+// if (value === 0) {
+//  console.log(value,"=value");
+// } else if(value>1){
+// console.log(value,">1");
+// } else if(value<-1){
+//     console.log(value,"<-1");
+// } else{
+//     console.log(value);
+// }
+// console.log("finish");
+
+
+// тернарный оператор
+// const age=20;
+// const result = age>=18 ? "Доступ разрешен":"Доступ запрещен";
+// console.log(result);
+
+
+//конструкция switch
+
+
+// switch ("mary") {
+//     case "ivan":
+//         console.log("inan");
+//         break;
+//         case "alex":
+//         console.log("alex");
+//         break;
+//         case "max":
+//             console.log("max");
+//             break;
+//     default:
+//         console.log("имя нет в списке");
+// }
+// console.log("finish");
+
+// let a=7;
+// let b=2;
+// let c=30;
+
+// console.log("a","b","c");
+// if (a>b) {
+//     a>c ? console.log("a"):console.log("c"); 
+// } 
+// else b>c? console.log("b"):console.log("c")
+// }
+ 
+//      console.log("закончили");
+
+let userRol="admin";
+switch (userRol) {
+    case "admin": 
+    console.log("это информация о администраторе роль,дата,напиток");
+        break;
+        case "manager": 
+        console.log(" это информация о менеджере роль,дата,напиток");
+        break;
+        case "user": 
+        console.log("это информация о пользователе роль,дата,напиток");
+        break;
+    default:
+        console.log("пользователя нет в списке");
+}
