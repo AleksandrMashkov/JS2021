3c2d39b3ea816f2141062347069bddc7ccef5c89
Aleksandr Mashkov <leonardodv_86@inbox.ru>
2021-10-23 12:49:08 +0300

    cw

--- JS/lesson6/class work/index.js
+++ JS/lesson6/class work/index.js
@@ -133,32 +133,91 @@
 
 // obj.sayHi("MAX");
 
-const obj={};
-obj.myFavoriteFilm = "оно";
-obj.age = '2019';
-obj[`name-rejiser`] = "не помню";
-obj.cantry ="Америка";
+// const obj={};
+// obj.myFavoriteFilm = "оно";
+// obj.age = '2019';
+// obj[`name-rejiser`] = "не помню";
+// obj.cantry ="Америка";
 
-console.log(obj.myFavoriteFilm,obj.age,obj['name-rejiser'],obj.cantry);
+// console.log(obj.myFavoriteFilm,obj.age,obj['name-rejiser'],obj.cantry);
 
-obj.proceeds="10000$"
-console.log(obj.proceeds);
- obj.getName=function (){
-    console.log("ОНО");
-}
-obj.getName();
-delete obj.age;
-console.log(obj);
+// obj.proceeds="10000$"
+// console.log(obj.proceeds);
+//  obj.getName=function (){
+//     console.log("ОНО");
+// }
+// obj.getName();
+// delete obj.age;
+// console.log(obj);
+
+// const str = "string";
+// console.log(str);
+// str[0] ="S";
+// console.log(str);
+
+// Сравнение строк
+// console.log( "Z".codePointAt(0) ); 
 
 
 
 
+//2) Реализовать функцию которая будет определять, 
+// в каком регистре записан n элемент переданной 
+// строки, если в верхнем то вернуть true, в противном случае 
+// вернуть false.
 
+// function isUpperCase(str, index) {
+//     return str.charAt(index).toUpperCase() === str.charAt(index);
+// }
+// console.log(isUpperCase('Приве МИР',0)); 
 
-// let user = {
-// 	sayName: function () {
-// 		console.log(“Ivan”)
-// 	}
+// const isUpperCase = (str, index)=>{
+//     if(index>str.lenght){
+//         console.log("не верный параметр");
+//     }
+//     if(str[index]===str[index].toUpperCase()){
+//         console.log(true);
+//     }else{
+//         console.log(false);
+//     }
 // };
-// user.sayName(); // Выведет Ivan
+// isUpperCase("Привет мир",0)
+
+//3) Реализовать функцию которая заменяет в строке str, все 
+// вхождения подстроки find, на подстроку replace.
+
+
+// const custemReplaseAll =(str,find,replaseStr)=>{
+    // while(str.includes(find)){
+    //     str = str.replace(find,replaseStr);
+    // }
+    // return str;
+    str = str.replace(find,replaseStr);
+    if(str.includes(find)){
+        custemReplaseAll(str,find,replaseStr);
+    }else{
+        return str;
+    }
+};
+console.log(custemReplaseAll("hi this is you, hi its me","hi","HELO"));
+
+// function includes(str,find,pos){
+
+//     let str="Привет мир"
+// }
+// console.log("Привет мир".includes("мир"));
+
+
+// 1)Реализовать функцию которая будет принимать числовой 
+// диапазон в качестве параметров [min, max] и будет 
+// возвращать случайное целое число из данного диапазона.
+
+// const getRandonNumber = (min,max)=>{
+// const rand = Math.random()*(max-min);
+// const result = min+rand;
+// console.log(result);
+// return Math.round(result);
+// // const result = min+
+// };
+// console.log(getRandonNumber(1,100));
 
